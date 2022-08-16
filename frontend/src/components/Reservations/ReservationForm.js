bd7c1dcd9f1c7ae97e7bb5e1daa9cce46339a08f
sharonmuwonge <64615640+shamuw@users.noreply.github.com>
2022-08-15 20:56:07 -0700

    Add Chakra UI

---
+++ frontend/src/components/Reservations/ReservationForm.js
@@ -0,0 +1,52 @@
+import { useState } from "react"
+
+const ReservationForm = () => {
+    const [arriveDate, setArriveDate] = useState('')
+    const [departDate, setDepartDate] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [checkedin, setCheckedin] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const reservation = {arriveDate, departDate, firstName, lastName, checkedin}
+
+        const response = await fetch('reservations', {
+            method: 'POST',
+            body: JSON.stringify(workout),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const json = await response.json()
+
+        if (!response.ok) {
+            setError(json.error)
+        }
+    }
+
+    return (
+        <form className="create">
+            <h2>Add new reservation</h2>
+
+            <label>First name:</label>
+            <input type="text" onChange={(e) => setFirstName(e.target.value)} value={firstName} />
+
+            <label>Last name:</label>
+            <input type="text" onChange={(e) => setLastName(e.target.value)} value={lastName} />
+            
+            <label>Arrival date:</label>
+            <input type="date" onChange={(e) => setArriveDate(e.target.value)} value={arriveDate} />
+
+            <label>Departure date:</label>
+            <input type="date" onChange={(e) => setDepartDate(e.target.value)} value={departDate} />
+
+            <label>Checked in:</label>
+            <input type="boolean" onChange={(e) => setCheckedin(e.target.value)} value={checkedin} />
+
+            <button>Add reservation</button>
+        </form>
+    )
+}
