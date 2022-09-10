e12e56c7a9b17596e941efba3ac1e03619f9a5e9
sharonmuwonge <64615640+shamuw@users.noreply.github.com>
2022-09-10 10:01:42 -0700

    Set up user login method and controlller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,25 @@
 const User = require('../models/userModel')
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'} )
+}
 
 // Login user
 const loginUser = async (req, res) => {
+    const {email, password} = req.body
 
-    res.json({mssg: 'login user'})
+    try {
+        const user = await User.login(email, password)
+
+        // create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 // Sign up user
@@ -16,7 +31,10 @@ const signupUser = async (req, res) => {
     try {
         const user = await User.signup(email, password)
 
-        res.status(200).json({email, user})
+        // create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -59,13 +59,36 @@ userSchema.statics.signup = async function(email, password) {
     const existingUser = await this.findOne({email})
 
     if (existingUser) {
-        throw Error('Username already in use.')
+        throw Error('Email already in use.')
     }
 
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(password, salt)
 
     const user = await this.create({email, password: hash})
+
+    return user
+}
+
+// Static signup method
+
+userSchema.statics.login = async function(email, password) {
+    
+    if (!email || !password) {
+        throw Error('All fields must be filled.')
+    }
+
+    const user = await this.findOne({email})
+
+    if (!user) {
+        throw Error('Incorrect email or password.')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) {
+        throw Error('Incorrect email or password.')
+    }
 
     return user
 }
